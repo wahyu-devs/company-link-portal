ca3167c280de135408fe3792a71d341637e214c6
Wahyu <wahyu_it@outlook.com>
2026-07-21 17:39:09 +0700

    Update email subject prefix

--- project-survey.config.js
+++ project-survey.config.js
@@ -2,5 +2,5 @@ window.PROJECT_SURVEY_GOOGLE_CONFIG = {
   clientId: "653624298946-ltjkbje28t4j05aa5rbv7q5e7bkt1u8j.apps.googleusercontent.com",
   folderId: "1AnVIlDzlZjPFioFTrMyXE0m2Ja0-LHVL",
   notifyTo: "wahyu@perkom.co.id",
-  subjectPrefix: "New Project Survey Form Upload",
+  subjectPrefix: "New Project Survey Form",
 };
